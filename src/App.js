6081532f37bf77bbe440fd9812afad5f34861e04
Lucy <lucybellott@gmail.com>
2023-12-12 16:44:18 -0500

    import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+  
+  const [image, setImage] = useState([])
+  
+  
+  
+  
   return (
-    <div className="App">
+   <div className="App">
+
       <h1>portfolio</h1>
+   
+   
+   
+   
     </div>
   );
 }
